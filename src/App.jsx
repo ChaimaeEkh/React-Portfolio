c53c2fdc5da0be93cf5905012e0bc78ab1093bf4
ChaimaeEkh <chaimaeelkhatib645@gmail.com>
2025-03-19 01:11:24 +0000

    n

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import './App.css'
+import Header from './components/header/Header';
+import Hero from './components/hero/Hero';
+import Main from './components/main/Main';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+      <Header/>
+      <Hero/>
+      <Main/>
+      <Contact/>
+      <Footer/>
+    </div>
   )
 }
 
