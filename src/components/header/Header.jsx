262d8b7a27c809419b1769427eea69d0b69d3746
ChaimaeEkh <chaimaeelkhatib645@gmail.com>
2025-03-22 23:16:02 +0000

    n

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,32 +1,60 @@
-import React from 'react';
-import './header.css';
+import React, { useState, useEffect } from "react";
+import "./header.css";
 
 export default function Header() {
+  const [showModal, setShowModal] = useState(false);
+  const closeIcon = "/fonts/close.png";
+  const moonIcon = "/fonts/moon.png";
+  const menuIcon = "/fonts/menu.png";
+  
+  
   return (
-    <header>
-      <button className='menu'>modal</button>
+    <header className="flex">
+      <button
+        onClick={() => {
+          setShowModal(true);
+        }}
+        className="menu"
+        aria-label="Open menu"
+      >
+        <img src={menuIcon} alt="Menu" />
+      </button>
+     
       <nav>
-        <ul className='flex'>
-          <li><a href="">About</a></li>
-          <li><a href="">Articles</a></li>
-          <li><a href="">Projects</a></li>
-          <li><a href="">Speaking</a></li>
-          <li><a href="">Contact</a></li>
+        <ul className="flex">
+          <li><a href="#">About</a></li>
+          <li><a href="#">Articles</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Speaking</a></li>
+          <li><a href="#">Contact</a></li>
         </ul>
       </nav>
-      <button>
-        light
+     
+      <button aria-label="Toggle dark mode">
+        <img src={moonIcon} alt="Dark mode" />
       </button>
-
-      <div className="fixed">
-          <ul className='modal border'>
-          <li><a href="">About</a></li>
-          <li><a href="">Articles</a></li>
-          <li><a href="">Projects</a></li>
-          <li><a href="">Speaking</a></li>
-          <li><a href="">Contact</a></li>
+     
+      {showModal && (
+        <div className="fixed">
+          <ul className="modal border">
+            <li>
+              <button
+                onClick={() => {
+                  setShowModal(false);
+                }}
+                aria-label="Close menu"
+              >
+                <img src={closeIcon} alt="Close" />
+              </button>
+            </li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Articles</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Speaking</a></li>
+            <li><a href="#">Contact</a></li>
           </ul>
-      </div>
+        </div>
+      )}
     </header>
-  )
+  );
 }
