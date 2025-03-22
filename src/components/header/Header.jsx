78469b6dffc222fb3fd5d3d3e1cdd8e415051635
ChaimaeEkh <chaimaeelkhatib645@gmail.com>
2025-03-22 17:36:46 +0000

    'header'

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,8 +3,19 @@ import './header.css';
 
 export default function Header() {
   return (
-    <div>
-      
-    </div>
+    <header>
+      <nav>
+        <ul className='flex'>
+          <li><a href="">About</a></li>
+          <li><a href="">Articles</a></li>
+          <li><a href="">Projects</a></li>
+          <li><a href="">Speaking</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+      </nav>
+      <button>
+
+      </button>
+    </header>
   )
 }
